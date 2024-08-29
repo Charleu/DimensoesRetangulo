3a932b4b9adf4240edfe101724ef5a487609afcc
Charleu <sdlimeira0@gmail.com>
2024-08-29 09:39:22 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DimensoesRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string separador = new string('.', 40);
+            Console.WriteLine("Informe a altura do retângulo:");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a largura do retângulo:");
+            double largura = double.Parse(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(altura, largura);
+
+            Console.WriteLine(separador);
+            Console.WriteLine($"Área do retângulo: {retangulo.CalculaArea().ToString("F2")}");
+            Console.WriteLine($"Perímetro do retângulo: {retangulo.CalculaPerimetro().ToString("F2")}");
+            Console.WriteLine($"Diagonal do retângulo: {retangulo.CalculaDiagonal().ToString("F2")}");
+            Console.WriteLine(separador);
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DimensoesRetangulo
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public Retangulo( double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double CalculaArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double CalculaPerimetro()
+        {
+            return (Altura * 2) + (Largura * 2);
+        }
+
+        public double CalculaDiagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
